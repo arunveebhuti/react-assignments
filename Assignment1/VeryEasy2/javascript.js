0f174f5cdec3bef8b25072877d4f85f2c66dcd33
Arun <arunveebhuti123@gmail.com>
2020-08-14 01:02:21 +0530

    very easy question 2

---
+++ Assignment1/VeryEasy2/javascript.js
@@ -0,0 +1,12 @@
+function dividesEvenly(a,b)
+ { 
+  if((a%b)==0)  {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+console.log(dividesEvenly(98,7));
+console.log(dividesEvenly(85,4));
+console.log(dividesEvenly(2,2));
