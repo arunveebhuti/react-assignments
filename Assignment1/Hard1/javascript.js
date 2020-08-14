98f7c31c14e13ef1861e715a54a38580b57b3726
Arun <arunveebhuti123@gmail.com>
2020-08-14 22:40:31 +0530

    final

--- Assignment1/Hard1/javascript.js
+++ Assignment1/Hard1/javascript.js
@@ -2,12 +2,12 @@
 
   function realType (temp){
   
-    return typeof(temp);
+    return Object.prototype.toString.call(temp);
 }
 console.log(realType (1));
 console.log(realType ("a"));
 console.log(realType (true));
-console.log(realType([1,2]));
+console.log(realType([]));
 console.log(realType(null));
 
 
