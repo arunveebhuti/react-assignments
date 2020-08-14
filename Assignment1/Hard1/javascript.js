ee157bdae22bef916d38ffdc7ea88df7cab216cf
Arun <arunveebhuti123@gmail.com>
2020-08-14 22:15:40 +0530

    Hard question 1

---
+++ Assignment1/Hard1/javascript.js
@@ -0,0 +1,16 @@
+
+
+  function realType (temp){
+  
+    return typeof(temp);
+}
+console.log(realType (1));
+console.log(realType ("a"));
+console.log(realType (true));
+console.log(realType([1,2]));
+console.log(realType(null));
+
+
+
+
+
